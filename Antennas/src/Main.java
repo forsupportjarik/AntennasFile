f54badd636bd077b7a4871d9bae66b22d184bc39
yakovalenko <jaroslawkovalenko@jmail.com>
2018-08-30 15:09:29 +0300

    new ant

--- Antennas/src/Main.java
+++ Antennas/src/Main.java
@@ -7,17 +7,20 @@ public class Main {
         String fileNameAntennas = "Antennas.txt";
         String fileNameXeusZte = "SiteFileForXeusZTE.xif";
         String fileNameXeusNokia = "SiteFileForXeusNokia.xif";
+        String fileNameXeusHuawei = "SiteFileForXeusHuawei.xif";
         String fileNameExportRpdb = "ExportFromRpdb.csv";
         String fileNameCoordinatesGsm = "Coordinates.txt";
         String pathForAntennas = "E:\\PhysicalData_UMTS\\" + fileNameAntennas;
         String pathForXeusZte = "//172.20.237.243//optimization//" + fileNameXeusZte;
         String pathForXeusNokia = "//172.20.237.243//optimization//" + fileNameXeusNokia;
+        String pathForXeusHuawei = "//172.20.237.243//optimization//" + fileNameXeusHuawei;
         String pathForGsm = "E:\\PhysicalData_GSM\\" + fileNameCoordinatesGsm;
         String pathToFileNameExportRpdb = "E:\\Teoco_supp\\" + fileNameExportRpdb;
 
         AntennasData antennasData = new AntennasData();
         SiteFileDataZte siteFileDataZte = new SiteFileDataZte();
         SiteFileDataNokia siteFileDataNokia = new SiteFileDataNokia();
+        SiteFileDataHuawei siteFileDataHuawei = new SiteFileDataHuawei();
         UpdateRpdbData updateRpdbData = new UpdateRpdbData();
         PerformingProcedures performingProcedures = new PerformingProcedures();
         CoordinatesFileGsm coordinatesFileGsm = new CoordinatesFileGsm();
@@ -36,6 +39,7 @@ public class Main {
 
         siteFileDataZte.writeDataToXeusFile(Utilities.createFileForXeus(pathForXeusZte));
         siteFileDataNokia.writeDataToXeusFileNokia(Utilities.createFileForXeus(pathForXeusNokia));
+        siteFileDataHuawei.writeDataToXeusFileHuawei(Utilities.createFileForXeus(pathForXeusHuawei));
 
     }
 }
